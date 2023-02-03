b1949536ed5acff143162b46601e20b81a12b2c8
chang-seop <ikavon@naver.com>
2023-02-03 17:35:06 +0900

    INSTALL: SpringBoot

---
+++ src/test/java/kr/kro/nineto6/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.nineto6.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
